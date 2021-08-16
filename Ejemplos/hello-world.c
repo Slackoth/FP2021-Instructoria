4ef2285bbcf383b128e48ccaa27c9180cbb0bc2e
Slackoth <00082318@uca.edu.sv>
2021-08-16 12:00:37 -0500

    hello-world example added

---
+++ Ejemplos/hello-world.c
@@ -0,0 +1,13 @@
+// studio.h file contains functions such as scanf()
+// and printf() to take input and display output
+// respectively
+#include <stdio.h>
+
+int main() {
+   // printf() displays the string inside quotation
+   // also mostly used for displaying formatted output
+   printf("Hello, World!\n");
+
+   //Exit status
+   return 0;
+}
